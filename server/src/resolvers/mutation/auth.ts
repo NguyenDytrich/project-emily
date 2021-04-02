013e24b4e52e91d43178346dca13531ae56c8505
Dytrich Nguyen <nguyen.dytrich@gmail.com>
2021-04-02 16:57:11 -0700

    Reject invalid password-confirmation pairs and invalid emails to RFC2822 spec

--- server/src/resolvers/mutation/auth.ts
+++ server/src/resolvers/mutation/auth.ts
@@ -1,13 +1,13 @@
 import { User } from '../../models';
 import bcrypt from 'bcrypt';
 
-class EmailError extends Error {
-  constructor(message) {
+export class EmailError extends Error {
+  constructor(message: string) {
     super(message);
   }
 }
 
-class PasswordError extends Error {
+export class PasswordError extends Error {
   constructor(message) {
     super(message);
   }
@@ -33,6 +33,12 @@ export async function signup(
 ): User {
   if (password !== passwordConf) {
     throw new PasswordError("Passwords don't match");
+  }
+
+  const RFC2822_EMAIL_REGEX = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+
+  if (!email.match(RFC2822_EMAIL_REGEX)) {
+    throw new EmailError('Invalid email address');
   }
 
   const passwordHash = await bcrypt.hash(
--- server/tests/unit/Auth.spec.ts
+++ server/tests/unit/Auth.spec.ts
@@ -1,5 +1,9 @@
 import { User } from '../../src/models';
-import { signup } from '../../src/resolvers/mutation/auth';
+import {
+  EmailError,
+  signup,
+  PasswordError,
+} from '../../src/resolvers/mutation/auth';
 import bcrypt from 'bcrypt';
 
 jest.mock('../../src/models');
@@ -19,4 +23,33 @@ describe('User signup', () => {
     await signup('fname', 'lname', 'user@test.com', 'password', 'password');
     expect(bcryptHash).toHaveBeenCalled();
   });
+  it('should reject invalid email addresses', async () => {
+    expect.hasAssertions();
+    try {
+      await signup(
+        'fname',
+        'lname',
+        'user(at)test.com',
+        'password',
+        'password',
+      );
+    } catch (e) {
+      expect(e).toBeInstanceOf(EmailError);
+      expect(e.message).toMatch('Invalid email address');
+    }
+  });
+  it('should reject unconfirmed password', async () => {
+    expect.hasAssertions();
+    try {
+      await signup('fname', 'lname', 'user@test.com', 'password', 'mismatched');
+    } catch (e) {
+      expect(e).toBeInstanceOf(PasswordError);
+      expect(e.message).toMatch("Passwords don't match");
+    }
+  });
+  it.todo('should reject duplicate email addresses');
+});
+
+describe('User retrieval', () => {
+  it.todo('instances should not have password information on retrieval');
 });
