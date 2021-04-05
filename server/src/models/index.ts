116ce076850fa98c930b03e7a2364f1cb4cc0fdb
Dytrich Nguyen <nguyen.dytrich@gmail.com>
2021-04-04 19:40:43 -0700

    fill out User model

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -14,11 +14,26 @@ async function initialize(url: string): Promise<void> {
 
   User.init(
     {
+      fname: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      lname: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
       email: {
         type: DataTypes.STRING,
         allowNull: false,
         unique: true,
       },
+      lastLogin: {
+        type: DataTypes.STRING,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false
+      }
     },
     {
       sequelize,
