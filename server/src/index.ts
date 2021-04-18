d134cfa379f77dcdd7c6e09989dfa6e79e83b2a2
Dytrich Nguyen <nguyen.dytrich@gmail.com>
2021-04-18 11:25:33 -0700

    Setup app to use express

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,29 +3,51 @@ import { initialize } from './models';
 import authChecker from './AuthChecker';
 import { resolvers } from './resolvers';
 import { buildSchema } from 'type-graphql';
-import { ApolloServer } from 'apollo-server';
+import { ApolloServer } from 'apollo-server-express';
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
-// TODO use dotenv to configure
-// Initialize Postgres connection
-initialize('postgres://testsuper@localhost:5432/test').then(async () => {
+// Entrypoint
+(async () => {
+  // TODO use dotenv to configure
+  // Initialize Postgres connection
+  await initialize('postgres://testsuper@localhost:5432/test');
+
+  // Build the schema
   const schema = await buildSchema({
     resolvers,
     authChecker,
   });
 
+  // Express configuration
+  const app = express();
+  app.use(
+    cors({
+      origin: 'http://localhost:3000',
+      credentials: true,
+    }),
+  );
+  app.use(cookieParser());
+  app.get('/ping', (_, res) => res.send('pong'));
+
+  // Set up Apollo with our schema
   const server = new ApolloServer({
     schema,
-    context: ({ req }) => {
-      const auth = req.headers.authorization || null;
-      return { auth };
-    },
+    // Map the { req, res } from express to our GraphQL context
+    context: ({ req, res }) => ({ req, res }),
   });
 
-  await server.listen();
-  console.log('Server live at localhost:4000');
-});
+  server.applyMiddleware({ app, cors: false });
+
+  // Go live
+  await app.listen(4000, () => {
+    console.log('Server live at localhost:4000');
+    console.log(`GraphQL live at localhost:4000${server.graphqlPath}`);
+  });
+})();
 
 export {};
