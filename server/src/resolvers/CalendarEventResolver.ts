6cdb426aeaeafbca1a3f24afa470ea3acac304e0
Dytrich Nguyen <nguyen.dytrich@gmail.com>
2021-05-10 08:42:41 -0700

    documentation

--- server/src/resolvers/CalendarEventResolver.ts
+++ server/src/resolvers/CalendarEventResolver.ts
@@ -31,6 +31,9 @@ class EventDetailsInput {
 
 @Resolver()
 export default class CalendarEventResolver {
+  /**
+   * @returns a list of all calendar events.
+   */
   @Query((returns) => [CalendarEvent])
   async events(): Promise<CalendarEvent[]> {
     const events = await CalendarEvent.findAll({
@@ -43,6 +46,12 @@ export default class CalendarEventResolver {
     return events;
   }
 
+  /**
+   * Creates a new event owned by the context's user
+   * @param {string} Title the event's title
+   * @param {string} description The event's description
+   * @param {Date} date The date of the event
+   */
   @Mutation(() => CalendarEvent)
   @UseMiddleware(AuthChecker)
   async createEvent(
@@ -58,6 +67,10 @@ export default class CalendarEventResolver {
     return event;
   }
 
+  /**
+   * Adds the current user to a list of attendees for an event. They will be marked as 'confirmed'.
+   * @param {number} eventId the id of the event
+   */
   @Mutation(() => String)
   @UseMiddleware(AuthChecker)
   async attendCalendarEvent(
@@ -82,6 +95,14 @@ export default class CalendarEventResolver {
     return 'OK';
   }
 
+  /**
+   * Adds a user to a list of participants for an event. Only the event owner can change participants.
+   * @param {number} userId The ID of the user to add as a participant
+   * @param {number} eventId The ID of the event to add a participant to
+   * @throws {Error} When no user can be found by the passed userId
+   * @throws {Error} When no calendar event ca nbe found by passed eventId
+   * @throws {Error} If the event is not owned by the current user
+   */
   @Mutation(() => String)
   @UseMiddleware(AuthChecker)
   async addCalendarEventParticipant(
