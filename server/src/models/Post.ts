47e4ddf5358d8e8d6f009094f3eb7d23d4b62c16
Dytrich Nguyen <nguyen.dytrich@gmail.com>
2021-05-18 17:11:48 -0700

    Add Posts using Quill-delta

---
+++ server/src/models/Post.ts
@@ -0,0 +1,32 @@
+import { Sequelize, DataTypes, Model } from Sequelize;
+
+import { Delta } from 'quill-delta';
+
+import { User } from './User';
+
+@ObjectType()
+export class Posts extends Model {
+  @Field()
+  public id!: number;
+
+  @Field((type) => User)
+  public author: User;
+
+  @Field()
+  public delta: Delta;
+}
+
+export default function initialize(sequelize: Sequelize): void {
+  Posts.init(
+    {
+      delta: {
+        type: DataTypes.ARRAY(DataTypes.JSON),
+        nullable: false,
+      },
+    },
+    {
+      sequelize,
+      underscored: true,
+    },
+  );
+}
