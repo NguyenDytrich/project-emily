08fc764eff3f08c2835fd02157df3322ab331ba5
Dytrich Nguyen <nguyen.dytrich@gmail.com>
2021-05-23 22:09:52 -0700

    Separate Posts into a component

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,28 +1,8 @@
 <template>
   <div class="grid grid-cols-3 grid-rows-2 h-full">
-    <div id="feed" class="bg-gray-100">
-      <a>Everyone</a>
-      <a>Friends</a>
-      <a>Groups</a>
-      <!-- Posts -->
-      <ul>
-        <li>
-          <div id="new-post" class="post">
-            <span class="flex items-baseline">Post Something</span>
-            <input class="px-5 py-2 w-full" placeholder="Create a post..." />
-          </div>
-        </li>
-        <li v-for="p in posts" :key="p.postId">
-          <div class="post">
-            <span class="flex items-baseline"
-              ><h2 class="text-lg">{{ p.author }}</h2>
-              <a class="text-xs text-gray-400 ml-2">{{ fdate(p.timestamp) }}</a>
-            </span>
-            <p class="px-5 py-2">{{ p.post }}</p>
-          </div>
-        </li>
-      </ul>
-    </div>
+    <Posts />
+
+    <!-- Jobs -->
     <div id="jobs">
       <a>Jobs For You</a>
       <ul>
@@ -45,7 +25,7 @@
         </li>
         <li>
           <div
-            class="bg-green-300 text-white rounded-xl shadow my-6 mx-6 hover:opacity-80"
+            class="bg-green-400 text-white rounded-xl shadow my-6 mx-6 hover:opacity-80"
           >
             <h2 class="text-lg text-center">more jobs</h2>
           </div>
@@ -75,7 +55,7 @@
         </li>
         <li>
           <div
-            class="bg-green-300 text-white rounded-xl shadow my-6 mx-6 hover:opacity-80"
+            class="bg-green-400 text-white rounded-xl shadow my-6 mx-6 hover:opacity-80"
           >
             <h2 class="text-lg text-center">more events</h2>
           </div>
@@ -87,27 +67,7 @@
 
 <script lang="ts">
 import { defineComponent } from "vue";
-
-const posts = [
-  {
-    author: "Abc Defg",
-    post: "Hello world",
-    postId: 0,
-    timestamp: Date.now(),
-  },
-  {
-    author: "Abc Defg",
-    post: "Test post please ignore",
-    postId: 1,
-    timestamp: Date.now(),
-  },
-  {
-    author: "Abc Defg",
-    post: "Test post 2 please ignore",
-    postId: 2,
-    timestamp: Date.now(),
-  },
-];
+import Posts from "../components/Posts.vue";
 
 const gigs = [
   {
@@ -138,10 +98,12 @@ const events = [
 ];
 
 export default defineComponent({
+  components: {
+    Posts,
+  },
   data() {
     return {
       events,
-      posts,
       gigs,
     };
   },
@@ -155,7 +117,7 @@ export default defineComponent({
 </script>
 
 <style scoped>
-#feed {
+#posts {
   @apply col-start-1;
   @apply col-end-3;
   grid-row-start: 1;
@@ -165,8 +127,8 @@ export default defineComponent({
 #jobs {
   grid-column-start: 3;
   grid-column-end: 3;
-	grid-row-start: 1;
-	grid-row-end: 2;
+  grid-row-start: 1;
+  grid-row-end: 2;
 }
 
 #events {
@@ -175,12 +137,5 @@ export default defineComponent({
   grid-row-end: last-line;
 }
 
-.post {
-  @apply bg-white;
-  @apply rounded-xl;
-  @apply shadow;
-  @apply my-6;
-  @apply mx-6;
-  @apply p-2;
-}
+
 </style>
