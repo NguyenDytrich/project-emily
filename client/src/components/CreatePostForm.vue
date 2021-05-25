68c177828066b07bb602b23c4c4fc5f23666ed98
Dytrich Nguyen <nguyen.dytrich@gmail.com>
2021-05-25 13:30:21 -0700

    Hook up form to createPost mutation in graphql

--- client/src/components/CreatePostForm.vue
+++ client/src/components/CreatePostForm.vue
@@ -1,17 +1,22 @@
 <template>
   <div class="">
     <h1>Create post</h1>
-    <div ref="editor" v-html="value"></div>
-    <div>Create</div>
+    <div ref="editor"></div>
+    <div @click="submit">Create</div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref } from "vue";
 import Quill from "quill";
+import axios from "axios";
+import { useStore } from "vuex";
+import { key } from "@/store";
+
 export default defineComponent({
   setup() {
-    return { editor: null };
+    const store = useStore(key);
+    return { editor: null, store };
   },
   mounted() {
     this.editor = new Quill(this.$refs.editor, {
@@ -25,19 +30,56 @@ export default defineComponent({
       formats: ["bold", "underline", "header", "italic"],
     });
 
-    this.editor.root.innerHTML = this.value;
     this.editor.on("text-change", (delta) => this.update(delta));
   },
   data() {
-    const value = ref("");
+    const delta = ref(null);
     return {
-      value,
+      delta,
     };
   },
   methods: {
-    update(delta) {
+    update() {
+      // Emit input event
       this.$emit("input", this.editor.getText() ? this.editor.rootHTML : "");
-      console.log(delta);
+      this.delta = this.editor.getContents();
+    },
+    async submit() {
+      if (this.delta == null) {
+        console.log("Empty form");
+        return;
+      }
+      try {
+        const res = await axios({
+          url: "http://localhost:4000/graphql",
+          method: "post",
+          data: {
+            query: `
+						mutation createPost($args:Delta!) {
+							createPost(delta: $args) {
+								id
+								author {
+									fname
+								}
+							}
+						}
+					`,
+            variables: {
+              args: JSON.stringify(this.delta),
+            },
+          },
+          headers: {
+            Authorization: this.store.getters.getAuth,
+          },
+        });
+        if (!res.data.errors) {
+          this.$emitter.emit("start-close-modal");
+        } else {
+          console.log(res.data);
+        }
+      } catch (err) {
+        console.log(err);
+      }
     },
   },
 });
